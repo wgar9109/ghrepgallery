607cdb51bd8baeead6539ed34dc61b6cc14eca48
wgar9109 <tylergary09@gmail.com>
2023-03-07 13:53:32 -0600

    API last call commits

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //this div class is where my profile information will appear
 const overview = document.querySelector(".overview");
-const username = "redrambles";
+const username = "wgar9109";
+const repoList = document.querySelector(".repo-list")
 
 const gitUserInfo = async function () {
     const userInfo = await
@@ -27,4 +28,22 @@ const displayUserInfo = function (data) {
     </div>
     `;
     overview.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await 
+    fetch(`https://api.github.com/users.${username}/repos?
+    sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const rpeo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classItem.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
